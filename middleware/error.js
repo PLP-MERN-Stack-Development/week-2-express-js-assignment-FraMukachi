ae01ca6e2eaec5655d87849fe64f0ec0566fcce9
FraMukachi <167871202+FraMukachi@users.noreply.github.com>
2025-07-09 08:08:40 +0200

    Create error.js

---
+++ middleware/error.js
@@ -0,0 +1,29 @@
+class NotFoundError extends Error {
+  constructor(message = 'Resource not found') {
+    super(message);
+    this.name = 'NotFoundError';
+    this.statusCode = 404;
+  }
+}
+
+class ValidationError extends Error {
+  constructor(message = 'Invalid input') {
+    super(message);
+    this.name = 'ValidationError';
+    this.statusCode = 400;
+  }
+}
+
+class UnauthorizedError extends Error {
+  constructor(message = 'Unauthorized') {
+    super(message);
+    this.name = 'UnauthorizedError';
+    this.statusCode = 401;
+  }
+}
+
+module.exports = {
+  NotFoundError,
+  ValidationError,
+  UnauthorizedError
+};
